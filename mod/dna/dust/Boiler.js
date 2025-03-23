9deb3e6308a2f514fae8f1176e2f0d9d65f77622
Anatolii Yakushko <shaddyx@gmail.com>
2025-03-23 03:02:54 +0200

    [+] added coal burning

--- mod/dna/dust/Boiler.js
+++ mod/dna/dust/Boiler.js
@@ -8,12 +8,19 @@ class Boiler {
             h:    0,
             temp:     0,
             pressure: 0,
+            waterAmount: 100,
+            energyPerSecond: 1,
             efficiency: 0.2,
         }, st) 
         this._heatIntegral = 0;
     }
 
     evo(dt) {
-        
+        const energy = lab.port.burner.energy * this.energyPerSecond * dt;
+        lab.port.burner.energy -= energy;
+        const effectiveEnergy = energy * this.efficiency;
+        const delta = effectiveEnergy / (this.waterAmount * this.energyPerSecond);
+        this.temp += delta;
+        lab.overlay.info.set('temp', this.temp)
     }
 }
--- mod/dna/dust/Burner.js
+++ mod/dna/dust/Burner.js
@@ -11,20 +11,20 @@ class Burner {
                 "coal": {
                     amount: 10,
                     // energy capacity
-                    capacity: 10,
+                    capacity: 10000,
                     // how much fuel of this type is burned per second
                     cps: 0.3
                 }
             }
         }, st) 
-        this._energy = 0;
+        this.energy = 0;
     }
 
 
     _burnFuel(dt, fuel) {
         const toBurn = fuel.cps * dt;
         const currentTickAmount = Math.min(fuel.amount, toBurn);
-        this._energy += currentTickAmount * fuel.capacity;
+        this.energy += currentTickAmount * fuel.capacity;
         fuel.amount -= currentTickAmount;
         return fuel.amount <= 0;
     }
@@ -32,12 +32,15 @@ class Burner {
     evo(dt) {
         const toDelete = [];
         for (const f in this.fuel) {
+            lab.overlay.info.set('f:' + f, this.fuel[f].amount)
             if (this._burnFuel(dt, this.fuel[f])){
                 toDelete.push(f);
             }
+            
         }
         for (const f of toDelete) {
             delete this.fuel[f];
         }
+        lab.overlay.info.set('energy', this.energy)
     }
 }
