07964da49b093ec5781537d5996b3eb84b36b5e1
Anatolii Yakushko <shaddyx@gmail.com>
2025-03-23 13:25:35 +0200

    [-] engine fixes

--- mod/dna/dust/Engine.js
+++ mod/dna/dust/Engine.js
@@ -11,18 +11,36 @@ class Engine {
             h:    0,
             // max pressure per second
             maxPps: 1000,
+            // pressure to push
+            ptp: 1,
             // current speed
             speed: 0,
-            speedPressureKoef: 1
+            speedPressureKoef: 1,
+            // 
+            friction: 0.05
         }, st) 
+        this.consumed = 0
+
     }
 
     evo(dt) {
-        
+        //res.mp3.steam_single.play();
         const pressure = Math.min(this.__.boiler.pressure, this.maxPps * dt);
         this.__.boiler.pressure -= pressure;
-        this.speed += pressure * this.speedPressureKoef
+        this.consumed += pressure;
+        if (this.consumed >= this.ptp) {
+            this.speed += this.consumed * this.speedPressureKoef;
+            this.consumed -= this.ptp;
+            // res.mp3.steam_single.play();
+        }
 
-        lab.overlay.info.set('speed', this.speed)
+        if (this.speed > 0) {
+            this.speed -= this.speed * this.friction * dt;
+        }
+
+        // res.mp3.steam_single.play();
+        //this.speed += pressure * this.speedPressureKoef
+
+        lab.overlay.info.set('speed', lib.math.round2(this.speed))
     }
 }
