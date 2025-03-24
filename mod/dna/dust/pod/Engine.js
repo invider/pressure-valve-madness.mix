8b8971cf094041e817c1248bedfd59ad5dd324d0
Anatolii Yakushko <shaddyx@gmail.com>
2025-03-24 22:02:07 +0200

    [-] fixed speed jitter

--- mod/dna/dust/pod/Engine.js
+++ mod/dna/dust/pod/Engine.js
@@ -15,7 +15,9 @@ class Engine {
             ptp: 1,
             // current speed
             speed: 0,
+            targetSpeed: 0,
             speedPressureKoef: 5,
+            speedGrowthPs: 20,
             // 
             friction: 0.05
         }, st) 
@@ -24,9 +26,9 @@ class Engine {
     }
 
     setSpeed(speed) {
-        this.speed = speed;
-        if (this.speed < 0) {
-            this.speed = 0;
+        this.targetSpeed = speed;
+        if (this.targetSpeed < 0) {
+            this.targetSpeed = 0;
         }
     }
 
@@ -36,18 +38,22 @@ class Engine {
         this.__.boiler.pressure -= pressure;
         this.consumed += pressure;
         if (this.consumed >= this.ptp) {
-            this.speed += this.consumed * this.speedPressureKoef;
+            this.targetSpeed += this.consumed * this.speedPressureKoef;
             this.consumed -= this.ptp;
             // res.mp3.steam_single.play();
         }
 
-        if (this.speed > 0) {
-            this.speed -= this.speed * this.friction * dt;
+        if (this.targetSpeed > 0) {
+            this.targetSpeed -= this.targetSpeed * this.friction * dt;
         }
-
+        const speedDiff = this.targetSpeed - this.speed;
+        if (speedDiff != 0) {
+            const speedSign = Math.abs(speedDiff) / speedDiff;
+            this.speed += speedSign * this.speedGrowthPs * dt
+        }
         // res.mp3.steam_single.play();
-        //this.speed += pressure * this.speedPressureKoef
+        //this.targetSpeed += pressure * this.targetSpeedPressureKoef
 
-        lab.overlay.info.set('speed', lib.math.round2(this.speed))
+        lab.overlay.info.set('speed', lib.math.round2(this.targetSpeed))
     }
 }
